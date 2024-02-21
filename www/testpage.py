eb1ecefae08794b4ff32064fa4faa1267e9da2d6
James Andrews <james@anagramic.co.uk>
2024-02-21 16:52:20 +0000

    updated webserver to have second pahe on /Page

--- www/testpage.py
+++ www/testpage.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
  
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Hello, World!"
+    return "<h1>Hello, World!</h1>"
+
+@app.route('/Page')
+def second_page():
+    return "<p>This is a second page</p>"
 
 if __name__ == "__main__":
     app.run(debug=True) 
