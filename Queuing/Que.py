3af0ee28821444cb28e0829321f94169b1908073
James Andrews <james@anagramic.co.uk>
2024-02-22 11:38:17 +0000

    Able to return a dict with the task list

--- Queuing/Que.py
+++ Queuing/Que.py
@@ -1,13 +1,41 @@
 import os
 
 def get_tasks():
-    os.chdir('Queuing/Tasks/')
-    return os.popen('ls').read().split()
+    os.chdir('/home/kali/Orion/Queuing/Tasks')
+    
+    #gets all the task folders in the "Tasks" directory and splits them in to an array
+    tasks = os.popen('ls').read().split()
+    tasks_array = []
+
+    #Goes throuhg all task folders and extraxcts the data (id,status,command,result) associated
+    for task in tasks:
+        os.chdir(task)
+        files = os.popen('ls').read().split()
+        
+        with open('STATUS') as file:
+            status = file.read()
+        
+        with open('COMMAND') as file:
+            command = file.read()
+        
+        if status == 'COMPLETE':
+            with open('OUTPUT') as file:
+                result = file.read()
+        else:
+            result = 'in progress'
+
+        tasks_array.append({'id':task,'status': status, 'command':command, 'result':result})
+        os.chdir('/home/kali/Orion/Queuing/Tasks')
+    
+    return tasks_array
 
 
 def new_task(command):
     tasks = get_tasks()
+    
+    for task in tasks:
+        print(task)
     os.system('pwd')
     return os.popen(command).read()
 
-print(new_task('ping -c 2 192.168.137.1'))
+print(new_task('ping -c 1 192.168.137.1'))
